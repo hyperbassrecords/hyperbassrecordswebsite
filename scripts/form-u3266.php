8e987a60f2300f251d266d548419752ae017f1d0
hyperbassrecords <61813878+hyperbassrecords@users.noreply.github.com>
2020-12-08 04:03:13 -0600

    2021

--- scripts/form-u3266.php
+++ scripts/form-u3266.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'hello@yourmail.com',
-		'to' => 'hello@yourmail.com'
+		'from' => 'hyperbassrecord@yandex.com',
+		'to' => 'hyperbassrecord@yandex.com'
 	),
 	'fields' => array(
 		'Email' => array(
